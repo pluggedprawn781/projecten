51dc45e63f00761fb21523a2ef9e69550d2e6481
pluggedprawn781 <45388595+pluggedprawn781@users.noreply.github.com>
2018-12-20 14:12:48 +0100

    Add files via upload

---
+++ martijn/project-2-vuurwerk/connect.php
@@ -0,0 +1,4 @@
+<?php
+ $connect = mysqli_connect('localhost','root','','vuurwerkwinkel')
+ or die('Error connecting to MySQL server.');
+?>
---
+++ martijn/project-2-vuurwerk/pakketten.php
@@ -0,0 +1,54 @@
+<?php
+include 'connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <title>firevork vuurwerkshop</title>
+      </head>
+      <body>
+        <center>
+          <h2>firevork vuurwerkshop</h2>
+        </center>
+        <section>
+          <ul>
+            <li><a href="/projecten/project-2-vuurwerk/vuurwerk_shop.php">Home</a></li>
+            <li><a href="/projecten/project-2-vuurwerk/knal.php">Knalvuurwerk</a></li>
+            <li><a href="/projecten/project-2-vuurwerk/sier.php">Siervuurwerk</a></li>
+            <li><a href="/projecten/project-2-vuurwerk/pakketten.php">pakketten</a></li>
+          </ul>
+        </section>
+        
+      <?php
+        $query = "SELECT * FROM tblproduct WHERE Soort = 'assortiment' ORDER BY id ASC";
+        $result = mysqli_query($connect, $query);
+        while($row = mysqli_fetch_array($result))
+        {
+      ?>
+       <div class="display">
+        <center>
+         <form method="post" action="">
+            <div class="item">
+              <img src="<?php echo $row["image"]; ?>" >
+              <h4><?php echo $row["name"]; ?></h4>
+              <h4>&euro;<?php echo $row["price"]; ?></h4>
+              <input class="aantal" type="text" name="quantity" value="1"><br><br>
+              <input class="addto" type="submit" name="in winkelwagen" value="in winkelwagen">
+            </div>
+         </form>
+         <center>
+       </div>
+       <?php
+        }
+       ?>
+      <div class="bottom-list">
+        <ul>
+          <li><a class="active" href="html_opdracht4.htm">FAQ</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    <footer>martijn van der waal    versie 1.0</footer>
+  </body>
+</html>
